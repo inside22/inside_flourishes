efd31c9a16c33bf5e66d2a8ebe809d90dd10abec
inside or A7mad <149911464+inside22@users.noreply.github.com>
2023-11-05 02:59:17 +0300

    Add files via upload

---
+++ inside_flourishes/README.md
@@ -0,0 +1,12 @@
+# inside_flourishes
+## Description
+Flourishes inside script 
+
+## Instructions to incorporate script:
+- Copy the script into a folder (to choose) from the 'resources' folder.
+- Add 'ensure inside_flourishes' in the 'Resources.cfg' document
+
+
+
+
+
---
+++ inside_flourishes/fxmanifest.lua
@@ -0,0 +1,12 @@
+author 'Inside <Discord:a77mmd:https://discord.gg/Xw3nUrQ2>'
+version '1.0'
+
+fx_version "adamant"
+lua54 "on"
+rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
+
+game "rdr3"
+
+client_scripts {
+	'client/client.lua',
+}
